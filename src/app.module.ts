c08322f0868e3b78980cba188510adc7f10c7835
André Lobo <loboandre@hotmail.com>
2024-10-16 13:23:53 -0400

    feat: add mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/barber-shop'),
+    AuthModule,
+    UsersModule,
+  ],
 })
 export class AppModule {}
